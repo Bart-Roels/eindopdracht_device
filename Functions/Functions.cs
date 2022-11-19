e0a92f2d9f7fc148d73668c05e88ad49a2d277d0
naam studentc:/Users/bart-/OneDrive/Documenten/Repo's/FSWD-Projecten/venv_fswd/Scripts/python.exe <bart.roels@student.howest.be>
2022-11-19 15:45:23 +0100

    Feat: error

--- Functions/Functions.cs
+++ Functions/Functions.cs
@@ -17,62 +17,81 @@ namespace MCT.Function
     public static class Functions
     {
         [FunctionName("AddLog")]
-        public static async Task<IActionResult> AddLog([HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "v1/addlog")] HttpRequest req,ILogger log)
+        public static async Task<IActionResult> AddLog([HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "v1/addlog")] HttpRequest req, ILogger log)
         {
-            log.LogInformation("C# HTTP trigger function processed a request.");
 
-            // Get request body
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-
-            // Deserialize JSON to Log object
-            Log logItem = JsonConvert.DeserializeObject<Log>(requestBody);
-
-            // Cosmos DB connection string
-            string connectionString = Environment.GetEnvironmentVariable("CosmosDB");
-
-            // Add to Cosmos DB
-            CosmosClientOptions options = new CosmosClientOptions()
+            try
             {
-                ConnectionMode = ConnectionMode.Gateway
-            };
+                log.LogInformation("C# HTTP trigger function processed a request.");
 
-            var cosmosClient = new CosmosClient(connectionString, options);
+                // Get request body
+                string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
 
-            // Create new guid for the id
-            logItem.Id = Guid.NewGuid();
-            var container = cosmosClient.GetContainer("hota", "logs");
-            await container.CreateItemAsync(logItem, new PartitionKey(logItem.HotAirBalloon));
-            return new OkObjectResult(logItem);
-            
+                // Deserialize JSON to Log object
+                Log logItem = JsonConvert.DeserializeObject<Log>(requestBody);
+
+                // Cosmos DB connection string
+                string connectionString = Environment.GetEnvironmentVariable("CosmosDB");
+
+                // Add to Cosmos DB
+                CosmosClientOptions options = new CosmosClientOptions()
+                {
+                    ConnectionMode = ConnectionMode.Gateway
+                };
+
+                var cosmosClient = new CosmosClient(connectionString, options);
+
+                // Create new guid for the id
+                logItem.Id = Guid.NewGuid();
+                var container = cosmosClient.GetContainer("hota", "logs");
+                await container.CreateItemAsync(logItem, new PartitionKey(logItem.HotAirBalloon));
+                return new OkObjectResult(logItem);
+
+            }
+            catch (System.Exception ex)
+            {
+                return new BadRequestObjectResult(ex);
+            }
+
         }
 
         [FunctionName("ReadLog")]
-        public static async Task<IActionResult> ReadLog([HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "v1/logs")] HttpRequest req,ILogger log)
+        public static async Task<IActionResult> ReadLog([HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "v1/logs")] HttpRequest req, ILogger log)
         {
-            // Cosmos DB connection string
-            string connectionString = Environment.GetEnvironmentVariable("CosmosDB");
-            
-            // Connect to Cosmos DB
-            CosmosClientOptions options = new CosmosClientOptions()
+            try
             {
-                ConnectionMode = ConnectionMode.Gateway
-            };
-            var cosmosClient = new CosmosClient(connectionString, options);
 
-            // Get all logs
-            var container = cosmosClient.GetContainer("hota", "logs");
-            var sqlQueryText = "SELECT * FROM c";
-            var queryDefinition = new QueryDefinition(sqlQueryText);
-            var iterator = container.GetItemQueryIterator<Log>(queryDefinition);
-            List<Log> logs = new List<Log>();
-            while (iterator.HasMoreResults)
+                // Cosmos DB connection string
+                string connectionString = Environment.GetEnvironmentVariable("CosmosDB");
+
+                // Connect to Cosmos DB
+                CosmosClientOptions options = new CosmosClientOptions()
+                {
+                    ConnectionMode = ConnectionMode.Gateway
+                };
+                var cosmosClient = new CosmosClient(connectionString, options);
+
+                // Get all logs
+                var container = cosmosClient.GetContainer("hota", "logs");
+                var sqlQueryText = "SELECT * FROM c";
+                var queryDefinition = new QueryDefinition(sqlQueryText);
+                var iterator = container.GetItemQueryIterator<Log>(queryDefinition);
+                List<Log> logs = new List<Log>();
+                while (iterator.HasMoreResults)
+                {
+                    var response = await iterator.ReadNextAsync();
+                    logs.AddRange(response.ToList());
+                }
+
+                return new OkObjectResult(logs);
+
+            }
+            catch (System.Exception ex)
             {
-                var response = await iterator.ReadNextAsync();
-                logs.AddRange(response.ToList());
+                return new BadRequestObjectResult(ex);
             }
 
-            return new OkObjectResult(logs);
-     
+
         }
     }
 }
